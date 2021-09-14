df31683e7ed93087f67d4ed4c4f9d5a533a805a3
WuHao <2323333339@qq.com>
2021-09-14 22:33:43 +0800

    feat: 创建仓库

---
+++ Plugin.php
@@ -0,0 +1,65 @@
+<?php
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+
+/**
+ * A Markdown Editor For Typecho
+ * 
+ * @package SMEditor
+ * @author HaoOuBa
+ * @version 1.0.0
+ * @link //78.al
+ */
+
+
+
+class SMEditor_Plugin implements Typecho_Plugin_Interface
+{
+  /**
+   * 激活插件方法,如果激活失败,直接抛出异常
+   * 
+   */
+  public static function activate()
+  {
+    Typecho_Plugin::factory('admin/write-post.php')->richEditor = array('SMEditor_Plugin', 'SMEdit');
+    Typecho_Plugin::factory('admin/write-page.php')->richEditor = array('SMEditor_Plugin', 'SMEdit');
+  }
+
+  /**
+   * 禁用插件方法,如果禁用失败,直接抛出异常
+   * 
+   */
+  public static function deactivate()
+  {
+  }
+
+
+  /**
+   * 获取插件配置面板
+   * 
+   */
+  public static function config(Typecho_Widget_Helper_Form $form)
+  {
+  }
+
+  /**
+   * 个人用户的配置面板
+   * 
+   */
+  public static function personalConfig(Typecho_Widget_Helper_Form $form)
+  {
+  }
+
+  /**
+   * 注入函数
+   * 
+   */
+  public static function SMEdit()
+  {
+    $version = '1.0.0';
+    $pluginUrl = Helper::options()->pluginUrl . '/SMEditor';
+    echo
+    <<<EOF
+      <link rel="stylesheet" href="$pluginUrl/assets/css/global.css?version=$version" />
+    EOF;
+  }
+}
