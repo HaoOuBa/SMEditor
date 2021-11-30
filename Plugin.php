cfec8d6c86e53b1c231d3daf1da236d03f50d0a1
WuHao <2323333339@qq.com>
2021-11-30 10:04:32 +0800

    fix: 更新

--- Plugin.php
+++ Plugin.php
@@ -6,14 +6,14 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
  * 
  * @package SMEditor
  * @author HaoOuBa
- * @version 1.2.4
+ * @version 1.2.5
  * @link //78.al
  */
 
 class SMEditor_Plugin implements Typecho_Plugin_Interface
 {
   public static $isDev = false;
-  public static $version = '1.2.4';
+  public static $version = '1.2.5';
 
   /**
    * 静态资源URL
